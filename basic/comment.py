744373885d81f58a875f9aa557eaaf9dc49bd89f
Ramesh Parthasarathy <msg.rameshp@gmail.com>
2017-03-23 16:14:25 -0400

    feat: Add basics
    - Add programming instructions for comment

---
+++ basic/comment.py
@@ -0,0 +1,11 @@
+# This is single line comment
+
+"""
+This is multi line comment
+with 3 double quotes to start and end
+"""
+
+'''
+This is also multi line comment
+with 3 single quotes to start and end
+'''
