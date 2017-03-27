d459adb7ccd908020c881cdb50560209a62ee413
Ramesh Parthasarathy <msg.rameshp@gmail.com>
2017-03-26 21:24:46 -0400

    feat: Add instructions
    Update instructions for print

--- basic/print.py
+++ basic/print.py
@@ -1,3 +1,51 @@
+# Python 3.6.0
+# Print some text
+print('Hello World!')
+# OUT: Hello World!
+
+# Print some text (alternate syntax)
+print("Hello World!")
+# OUT: Hello World!
+
+# Print variable
+a = 10
+print(a)
+# OUT: 10
+
+# Print text and variable
+a = 10
+print('The value of a is', a)
+# OUT: The value of a is 10
+
+# Print text and variable
+age = 'He is %d years old' % 10
+print(age)
+# OUT: He is 10 years old
+
+# Print formatted text and variable
+cost = 'The cost is $%.2f' % 10
+print(cost)
+# OUT: The cost is $10.00
+
+# Print two variables
+name = 'Tom'
+greeting = 'Good Morning'
+print(greeting,',', name,'!')
+# OUT: Good Morning , Tom !
+
+# Print two variables w/o white space
+name = 'Tom'
+greeting = 'Good Morning'
+print(greeting, name, sep=', ', end='!')
+# OUT: Good Morning, Tom!
+
+
+
+
+
+
+
+
 # Python 2
 print 1, 2, 3
 # 1 2 3
