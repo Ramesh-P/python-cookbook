ac584cf87ea319fb3187a6e5f8a03daa4ebee5bf
Ramesh Parthasarathy <msg.rameshp@gmail.com>
2017-04-01 16:44:32 -0400

    feat: Add instructions
    - Add instructions for user defined functions

--- basic/function.py
+++ basic/function.py
@@ -39,3 +39,57 @@ announce('April')
 # OUT: Today is 1st April, 2017
 announce('April', '2018')
 # OUT: Today is 1st April, 2018
+
+# Python 3.6.0
+# Variable length arguments (undefined), No return
+def items(fruit, *others):
+    print(fruit)
+    for item in others:
+        print(item)
+
+items('Apple')
+# OUT: Apple
+items('Apple', 'Orange', 'Mango')
+# OUT: Apple, Orange, Mango
+
+# Python 3.6.0
+# Variable with global scope, No return
+total = 0
+def assign(number):
+    total = number
+    print('Value of local variable total is', total)
+
+assign(10)
+print('Value of global variable total is', total)
+# OUT:
+    # Value of local variable total is 10
+    # Value of global variable total is 0
+
+# Python 3.6.0
+# Variable with global scope, With return
+fruit = 'Apple'
+def assign(name):
+    fruit = name
+    print('Value of local variable fruit is', fruit)
+    return fruit
+
+assign('Orange')
+print('Value of global variable fruit is', fruit)
+# OUT:
+    # Value of local variable fruit is Orange
+    # Value of global variable fruit is Apple
+
+# Python 3.6.0
+# Variable declared as global, No return
+total = 0
+print('Value of variable before the function call is', total)
+
+def assign(number):
+    global total
+    total = number
+
+assign(10)
+print('Value of variable after the function call is', total)
+# OUT:
+    # Value of variable before the function call is 0
+    # Value of variable after the function call is 10
