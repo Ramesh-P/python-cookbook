35aae8e7926386f94d04c67c61fa1f4f9667269a
rohan-ramesh <mcboat@icloud.com>
2017-04-06 14:05:17 -0400

    Various miscellaneous updates
    - Updated index
    - Improved search
    - Added link to site in README
    - Improved responsive design
    - Improved accessibility
    - Witty comment in main.less, if you’re into that sort of thing

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# python-cookbook
+# [Python Cookbook](https://ramesh-p.github.io/python-cookbook)
 This is a self learning language guide for Python programming with plenty of examples. It is suitable for both beginner and professional developers.
--- search.js
+++ search.js
@@ -2,27 +2,33 @@ var client = algoliasearch("B6KQUPHS9M", "bc54b1bf2df17f1a41dd23cae387af02");
 var index = client.initIndex("recipes");
 var rawFile = new XMLHttpRequest();
 
-document.getElementById("search").onkeypress = function () {
-    index.search(
-        document.getElementById("search").value,
-        function searchDone(err, content) {
-            if (err) {
-                console.error(err);
-                return;
-            }
+document.getElementById("search").onkeypress = _.debounce(function () {
+    if (document.getElementById("search").value) {
+        index.search(
+            document.getElementById("search").value,
+            {facets: document.getElementById("sel").value},
+            function searchDone(err, content) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
 
-            document.getElementById("results").innerHTML = "";
-            for (var h in content.hits) {
-                rawFile.open("GET", content.hits[h].folder + "/" + content.hits[h].file, false);
-                rawFile.send();
-                document.getElementById("results").innerHTML +=
-                "<li><h3><a href='" + content.hits[h].folder + "/" + content.hits[h].file + "'>" + content.hits[h].friendlyName + "</a></h3><code class='prettyprint'>" + rawFile.responseText + "</code></li>";
+                document.getElementById("results").innerHTML = "";
+                for (var h in content.hits) {
+                    rawFile.open("GET", content.hits[h].folder + "/" + content.hits[h].file, false);
+                    rawFile.send();
+                    document.getElementById("results").innerHTML +=
+                    "<li><h3><a href='" + content.hits[h].folder + "/" + content.hits[h].file + "'>" + content.hits[h]._highlightResult.friendlyName.value + "</a></h3><code class='prettyprint'>" + rawFile.responseText + "</code></li>";
+                }
+                PR.prettyPrint();
             }
-            PR.prettyPrint();
-        }
-    );
-};
+        );
+    } else {
+        document.getElementById("results").innerHTML = "";
+    }
+}, 1);
 
 document.getElementById("search").onkeydown = function (event) {
-    if (event.keycode === 8) document.getElementById("search").onkeypress();
+    if (event.keyCode === 8) document.getElementById("search").onkeypress();
 };
+document.getElementById("sel").click = document.getElementById("search").onkeypress;
